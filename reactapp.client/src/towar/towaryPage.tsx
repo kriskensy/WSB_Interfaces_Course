fd2e6d1fc1eeaf0e628c1cbc3cfd23121829989e
kriskensy <kriskensy@proton.me>
2024-05-24 18:36:52 +0200

    Lecture 4, hooks added

--- reactapp.client/src/towar/towaryPage.tsx
+++ reactapp.client/src/towar/towaryPage.tsx
@@ -4,8 +4,73 @@ import Accordion from 'react-bootstrap/Accordion';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { Towar } from './towarPage';
+import { useEffect, useState } from 'react';
 
 export const Towary = () => {
+
+    //hook useState zwraca 2 elementy: po pierwsze dane, ktore przechowuje, oraz funckje do aktualizacji tych danych
+    //dane nazwalismy listaTowarow i funkcje nazwalismy setListTowarow
+
+    const [listaTowarow, setListaTowarow] = useState<Towar[]>([
+        //useState to hook, ktory przetrzymuje dane destrykturyzujac go zwracamy dane i funkcje do ustawiania tych danych
+
+        {
+            kod: "1",
+            nazwa: "NazwaTowaru1",
+            kategoria: "Kategoria1",
+            pkwiu: "1",
+            cena: 1,
+            iloscSztuk: 1
+        },
+        {
+            kod: "2",
+            nazwa: "NazwaTowaru2",
+            kategoria: "Kategoria2",
+            pkwiu: "2",
+            cena: 2,
+            iloscSztuk: 2
+        },
+        {
+            kod: "3",
+            nazwa: "NazwaTowaru3",
+            kategoria: "Kategoria3",
+            pkwiu: "3",
+            cena: 3,
+            iloscSztuk: 3
+        },
+        {
+            kod: "4",
+            nazwa: "NazwaTowaru4",
+            kategoria: "Kategoria4",
+            pkwiu: "4",
+            cena: 4,
+            iloscSztuk: 4
+        },
+    ]);
+
+    useEffect(() => {//useEffect to hook, ktory wywoluje sie w momencie zmiany wartosci wprowadzonej do tablicy. gdy tablica jest pusta wywoluje sie w momencie ladowania komponentow
+        const listaTowarowPobranaZAPI: Towar[] = [
+            {
+                kod: "1API",
+                nazwa: "NazwaTowaru1API",
+                kategoria: "Kategoria1API",
+                pkwiu: "1API",
+                cena: 1,
+                iloscSztuk: 1
+            },
+            {
+                kod: "2API",
+                nazwa: "NazwaTowaru2API",
+                kategoria: "Kategoria2API",
+                pkwiu: "2API",
+                cena: 2,
+                iloscSztuk: 2
+            }];
+
+        setListaTowarow(listaTowarowPobranaZAPI);
+    }, [])
+
     return (
         <Container fluid>
             <Accordion defaultActiveKey="0">
@@ -60,30 +125,16 @@ export const Towary = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>Towar 1</td>
-                        <td>Kategoria 1</td>
-                        <td>111</td>
-                        <td>111</td>
-                        <td>1</td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Towar 2</td>
-                        <td>Kategoria 2</td>
-                        <td>222</td>
-                        <td>222</td>
-                        <td>2</td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td>Towar 3</td>
-                        <td>Kategoria 3</td>
-                        <td>333</td>
-                        <td>333</td>
-                        <td>3</td>
-                    </tr>
+                    {listaTowarow.map((item, index) =>
+                        <tr key={index}>
+                            <td>{item.kod}</td>
+                            <td>{item.nazwa}</td>
+                            <td>{item.kategoria}</td>
+                            <td>{item.pkwiu}</td>
+                            <td>{item.cena}</td>
+                            <td>{item.iloscSztuk}</td>
+                        </tr>
+                    )}
                 </tbody>
             </Table>
         </Container>
---
+++ reactapp.client/src/types/towar/towar.ts
@@ -0,0 +1,8 @@
+export type Towar = {
+    kod: string;
+    nazwa: string;
+    kategoria: string;
+    pkwiu: string;
+    cena: number;
+    iloscSztuk: number;
+}
